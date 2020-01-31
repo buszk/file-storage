afd1302550b237178923ff2306f0ad14287b2c57
Zekun Shen <17345356+buszk@users.noreply.github.com>
2020-01-31 12:34:35 -0500

    test readability

--- tests/test_server.rs
+++ tests/test_server.rs
@@ -26,20 +26,30 @@ fn test_server() {
     let mut p = Command::new("target/debug/file-storage")
                         .spawn()
                         .expect("failed to start server");
-    
+    let dir = String::from("files");
+    let upload_fname = String::from ("gi");
+    let fname = String::from(".gitignore");
     /* Upload */
-    remove_file_no_throw(String::from("files/gi"));
-    execute(String::from("curl -sS --upload-file .gitignore 127.0.0.1:8000/upload/gi")).wait().expect("curl failed");
-    assert!(diff(".gitignore", "files/gi"));
+    remove_file_no_throw(format!("{}/{}", dir, upload_fname));
+    
+    execute(format!("curl -sS --upload-file {} 127.0.0.1:8000/upload/{}", fname, upload_fname))
+            .wait()
+            .expect("curl upload failed");
+    assert!(diff(fname.as_str(), format!("{}/{}", dir, upload_fname).as_str()));
     
     /* Download */
-    remove_file_no_throw(String::from("files/foo.txt"));
+    let upload_fname = String::from("foo.txt");
+    remove_file_no_throw(format!("{}/{}", dir, upload_fname));
     remove_file_no_throw(String::from("temp"));
-    let mut file = File::create("files/foo.txt").unwrap();
+
+    let mut file = File::create(format!("{}/{}", dir, upload_fname)).unwrap();
     file.write_all(b"Hello, world!").unwrap();
-    execute(String::from("curl -sS 127.0.0.1:8000/file/foo.txt -o temp")).wait().expect("Unable to crul");
+    execute(format!("curl -sS 127.0.0.1:8000/file/{} -o temp", upload_fname))
+            .wait()
+            .expect("crul download failed");
     assert!(diff("temp", "files/foo.txt"));
-    remove_file_no_throw(String::from("files/foo.txt"));
+
+    remove_file_no_throw(format!("{}/{}", dir, upload_fname));
     remove_file_no_throw(String::from("temp"));
 
     /* Stop server */
