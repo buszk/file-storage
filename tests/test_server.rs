727235e4eae31f0f7de09edbd7126137e63ffb76
buszk <17345356+buszk@users.noreply.github.com>
2020-01-31 23:46:07 -0500

    travis cat

--- tests/test_server.rs
+++ tests/test_server.rs
@@ -64,7 +64,6 @@ fn test_download() {
     let dir = String::from("files");
     /* Download */
     let upload_fname = String::from("test");
-    remove_file_no_throw(format!("{}/{}", dir, upload_fname));
     remove_file_no_throw(String::from("temp"));
 
     execute(format!("curl -sS 127.0.0.1:8082/file/{} -o temp", upload_fname))
